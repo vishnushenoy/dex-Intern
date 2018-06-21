7da7bb0540d4b1ae17f0f8ad52abf6e702ca540b
Vishnu Shenoy <vishnushenoy09@gmail.com>
2018-06-21 16:40:34 +0530

    class file

---
+++ models.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+
+
+class swapp:
+    def __init__(self, x, y, time):
+        self._x = x
+        self._y = y
+        self._time = time
+
+    def getx(self):
+        return self._x
+
+    def gety(self):
+        return self._y
+
+    def gettime(self):
+        return self._time
+
+    def getNode(self):
+        return [self._x, self._y, self._time]
+
+    # def setxy(self, x, y):
+    #     self._x = x
+    #     self._y = y
