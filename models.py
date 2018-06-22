3624262e7b9c8fb718632457f071c2423d13f391
althafahammedpp <althafahammadpp@gmail.com>
2018-06-22 17:29:13 +0530

    models modified

--- models.py
+++ models.py
@@ -21,3 +21,25 @@ class swapp:
 
     def getxy(self):
         return (self._x, self._y)
+
+
+class package:
+    def __init__(self, demand_loc, supply_loc, package_id):
+        self._demand_loc = demand_loc
+        self._supply_loc = supply_loc
+        self._package_id = package_id
+
+    def getdemandloc(self):
+        return self._demand_loc
+
+    def getsupplyloc(self):
+        return self._supply_loc
+
+    def getpackageid(self):
+        return self._package_id
+
+    def getpackagedetails(self):
+        return [self._supply_loc, self._demand_loc, self._package_id]
+
+    def getsupplyanddemand(self):
+        return [self._supply_loc, self._demand_loc]
